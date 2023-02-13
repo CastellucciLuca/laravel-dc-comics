f0b92443523fbe759b782164fe58f2154ce4532c
CastellucciLuca <castellucciluca98@gmail.com>
2023-02-13 16:46:01 +0100

    Create COMICS Seeder and print given Comics data

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.app')
+
+@section('main-app')
+<main>
+    <h1>Dc Comics</h1>
+    <table class="table table-hover">
+        <thead>
+        <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Title</th>
+            <th scope="col">Description</th>
+            <th scope="col">Price</th>
+            <th scope="col">Series</th>
+            <th scope="col">Release Date</th>
+            <th scope="col">Type</th>
+            <th scope="col"></th>
+        </tr>
+        </thead>
+        <tbody>
+            @forelse ($comicList as $comic)
+            <tr>
+                <th scope="row">{{$comic->id}}</th>
+                <td>{{$comic->title}}</td>
+                <td>{{$comic->description}}</td>
+                <td>{{$comic->price}}</td>
+                <td>{{$comic->series}}</td>
+                <td>{{$comic->sale_date}}</td>
+                <td>{{$comic->type}}</td>
+                <td>
+                    <a href="" class="btn btn-primary">View</a>
+                </td>
+            </tr>
+            @empty
+
+            @endforelse
+        </tbody>
+    </table>
+</main>
+@endsection
