2c725caa3d8c93deba834e21e7481d22470f028f
CastellucciLuca <castellucciluca98@gmail.com>
2023-02-14 17:17:15 +0100

    Implementazione dei fillable per salvare i dati nel db

--- app/Http/Controllers/Admin/ComicsController.php
+++ app/Http/Controllers/Admin/ComicsController.php
@@ -39,13 +39,7 @@ class ComicsController extends Controller
     {
         $data = $request->all();
         $newComic = new Comic();
-        $newComic->title = $data['title'];
-        $newComic->description = $data['description'];
-        $newComic->thumb = $data['thumb'];
-        $newComic->price = $data['price'];
-        $newComic->series = $data['series'];
-        $newComic->sale_date = $data['sale_date'];
-        $newComic->type = $data['type'];
+        $newComic->fill($data);
         $newComic->save();
         return redirect()->route('comic.show', $newComic->id);
     }
