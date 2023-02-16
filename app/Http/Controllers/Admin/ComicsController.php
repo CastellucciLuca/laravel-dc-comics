1504f8b59080919ca14646a3e017606f94ebc3e1
CastellucciLuca <castellucciluca98@gmail.com>
2023-02-16 16:18:05 +0100

    Change validate error messages

--- app/Http/Controllers/Admin/ComicsController.php
+++ app/Http/Controllers/Admin/ComicsController.php
@@ -41,12 +41,33 @@ class ComicsController extends Controller
         $request->validate([
             'title' => 'required|string|min:2|max:255',
             'description' => 'required|min:10',
-            'thumb' => 'required|url|min:2',
+            'thumb' => 'required|url|min:10',
             'price' => 'required|numeric',
             'series' => 'required|string|min:2|max:255',
             'sale_date' => 'required|date',
             'type' => 'required|string|min:2|max:60',
-        ]);
+        ],
+        [
+            'title.required' => 'inserisci un Titolo',
+            'title.min' => 'il Titolo deve essere di almeno 2 caratteri',
+            'title.max' => 'il Titolo deve essere di massimo 255 caratteri',
+            'description.required' => 'inserisci una Descrizione',
+            'description.min' => 'la Descrizione deve essere di almeno 10 caratteri',
+            'thumb.required' => 'inserisci una Thumbnail',
+            'thumb.url' => 'inserisci un URL corretto nella Thumbnail',
+            'thumb.min' => 'inserisci un URL di almeno 10 caratteri',
+            'price.required' => 'inserisci un Prezzo',
+            'price.numeric' => 'il Prezzo deve essere composto da valori Numerici',
+            'series.required' => 'inserisci una Serie',
+            'series.min' => 'la Serie deve essere di almeno 2 caratteri',
+            'series.max' => 'la Serie deve essere di massimo 255 caratteri',
+            'sale_date.required' => 'inserisci una Data di uscità',
+            'sale_date.date' => 'la Data di uscità deve essere una data',
+            'type.required' => 'Inserisci un Tipo di fumetto',
+            'type.min' => 'il Tipo di fumetto deve essere di almeno 2 caratteri',
+            'type.max' => 'il Tipo di fumetto deve essere di massimo 255 caratteri'   
+        ]
+    );
         $newComic = new Comic();
         $newComic->fill($data);
         $newComic->save();
