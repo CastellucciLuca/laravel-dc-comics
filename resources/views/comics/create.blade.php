9ff0a24b29126e5319037271aebda658aa9d5317
CastellucciLuca <castellucciluca98@gmail.com>
2023-02-16 15:51:29 +0100

    Save old field that was written in the form after receiving an error

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -24,49 +24,49 @@
                             <label class="form-label">
                                 Title
                             </label>
-                            <input type="text" class="form-control" name="title"  >
+                            <input type="text" class="form-control" name="title" value="{{$comic->title ?? old('title')}}" >
                         </div>
 
                         <div class="mb-3">
                             <label class="form-label">
                                 Description
                             </label>
-                            <textarea name="description" class="form-control" id="" ></textarea>
+                            <textarea name="description" class="form-control" value="{{$comic->description ?? old('description')}}"></textarea>
                         </div>
 
                         <div class="mb-3">
                             <label class="form-label">
                                 Thumb
                             </label>
-                            <input type="text" class="form-control" name="thumb">
+                            <input type="text" class="form-control" name="thumb" value="{{$comic->thumb ?? old('thumb')}}">
                         </div>
 
                         <div class="mb-3">
                             <label class="form-label">
                                 Price
                             </label>
-                            <input type="text" class="form-control" name="price">
+                            <input type="text" class="form-control" name="price" value="{{$comic->price ?? old('price')}}">
                         </div>
 
                         <div class="mb-3">
                             <label class="form-label">
                                 Series
                             </label>
-                            <input type="text" class="form-control" name="series">
+                            <input type="text" class="form-control" name="series" value="{{$comic->series ?? old('series')}}">
                         </div>
 
                         <div class="mb-3">
                             <label class="form-label">
                                 Sale Date
                             </label>
-                            <input type="text" class="form-control" name="sale_date">
+                            <input type="text" class="form-control" name="sale_date" value="{{$comic->sale_date ?? old('sale_date')}}">
                         </div>
 
                         <div class="mb-3">
                             <label class="form-label">
                                 Type
                             </label>
-                            <input type="text" class="form-control" name="type">
+                            <input type="text" class="form-control" name="type" value="{{$comic->type ?? old('type')}}">
                         </div>
                         <button type="submit" class="btn btn-primary">
                             Submit
